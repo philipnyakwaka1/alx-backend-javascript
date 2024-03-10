e04be9776d375a5d8a8372df5fa8fa90c086183c
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-03-10 12:53:22 +0300

    to create tests and lint

--- 0x00-ES6_basic/0-constants.test.js
+++ 0x00-ES6_basic/0-constants.test.js
@@ -1,5 +1,4 @@
 import { taskFirst, taskNext } from './0-constants';
-test(`checks if output of taskFirst to be ${taskFirst()}`, ()=>{ 
-    const result = taskFirst();
-    expect(result).toBe('I prefer const when I can.');
- })
+test('tasks are correctly defined', () => {
+    expect(`${taskFirst()} ${taskNext()}`).toEqual("I prefer const when I can. But sometimes let is okay");
+  });
