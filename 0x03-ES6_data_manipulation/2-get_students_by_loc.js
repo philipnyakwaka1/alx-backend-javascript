f2f7586a5b8e266fb91c0994ba432fecd06ab98b
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-03-12 05:26:28 +0300

    reduce

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,3 @@
 export default function getStudentsByLocation(students, city) {
-    if(Array.isArray(students) && typeof(city) === 'string') {
-        return students.filter(student => student.location === city);
-    }
+  return (Array.isArray(students) && typeof (city) === 'string') ? students.filter((student) => student.location === city) : null;
 }
