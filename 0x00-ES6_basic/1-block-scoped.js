2d6cf3dd90dc09147a1ce272520cd042ca532693
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-03-12 05:00:27 +0300

    full test

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,5 @@
-export default function taskBlock(trueOrFalse) {
+export default function taskBlock() {
   const task = false;
   const task2 = true;
-
-  if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
-  }
-
   return [task, task2];
 }
