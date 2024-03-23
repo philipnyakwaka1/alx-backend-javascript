1e8f66c2e63e576a322a5d5f7dcc5e2fe681513a
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-03-23 19:30:06 +0300

    create promise that resolves or reject based on boolean nature of success argument

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,6 @@
 import { createUser, uploadPhoto } from './utils';
 
-export default async function handleProfileSignup() {
+export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()]).then((results) => {
     const [photo, user] = results;
     console.log(photo.body, user.firstName, user.lastName);
