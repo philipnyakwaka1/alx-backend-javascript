90853e9cc99a847978fffa75fcffdf1664319775
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-03-23 11:31:11 +0300

    modify budget

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,8 +1,9 @@
 export default function getBudgetObject(income, gdp, capita) {
-  const budget = { 
-    income, 
+  const budget = {
+    income,
     gdp,
-    capita };
+    capita,
+  };
 
   return budget;
 }
