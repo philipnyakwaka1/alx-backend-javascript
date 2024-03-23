2003c76b4125b98f580c74f0cfec69cc691dc6c9
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-03-23 19:19:47 +0300

    create promise that resolves or reject based on boolean nature of success argument

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import { createUser, uploadPhoto } from './utils';
+
+export default async function handleProfileSignup() {
+  try {
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+    console.log(photo.body, user.firstName, user.lastName);
+  } catch (error) {
+    console.log('Signup system offline');
+  }
+}
