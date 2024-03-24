d224c5b54f173039e7320f55b6e3b72eb7e16fe7
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-03-24 16:19:39 +0300

    created object footprint with private attribute

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,43 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._length;
+  }
+
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('name must be  a string');
+    }
+  }
+
+  set length(length) {
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw new TypeError('length must be  a number');
+    }
+  }
+
+  set students(students) {
+    if (Array.isArray(students) && students.every((item) => typeof item === 'string')) {
+      this._students = students;
+    } else {
+      throw new TypeError('students must be an array');
+    }
+  }
+}
