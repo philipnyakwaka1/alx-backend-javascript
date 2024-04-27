2e511c897a77309040a61b7f33676a2dff2028b1
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-04-27 13:44:54 +0300

    flat

--- 0x00-ES6_basic/12-main.js
+++ 0x00-ES6_basic/12-main.js
@@ -1,11 +1,17 @@
+import createIteratorObject from "./100-createIteratorObject.js";
+
 import createEmployeesObject from './11-createEmployeesObject.js';
 import createReportObject from './12-createReportObject.js';
 
 const employees = {
     ...createEmployeesObject('engineering', ['Bob', 'Jane']),
     ...createEmployeesObject('marketing', ['Sylvie'])
-};      
+};
 
 const report = createReportObject(employees);
-console.log(report.allEmployees);
-console.log(report.getNumberOfDepartments(report.allEmployees));
+
+const reportWithIterator = createIteratorObject(report);
+
+for (const item of reportWithIterator) {
+    console.log(item);
+}
