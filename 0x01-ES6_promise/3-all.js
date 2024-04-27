8385613d539bb387234bbc2e41541b85226b04db
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-04-27 17:28:49 +0300

    eslint

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { createUser, uploadPhoto } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()]).then((results) => {
     const [photo, user] = results;
-    console.log(photo.body, user.firstName, user.lastName);
+    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
   })
     .catch(() => {
       console.log('Signup system offline');
