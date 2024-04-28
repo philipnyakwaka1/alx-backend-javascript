bad9466d57f32ffbc37d0b5c287c28b64f35d495
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-04-28 11:00:08 +0300

    filter city

--- 0x03-ES6_data_manipulation/0-get_list_students.test.js
+++
@@ -1,9 +0,0 @@
-import getListStudents from './0-get_list_students';
-
-describe('getListStudents function', () => {
-  it('array objects', () => {
-    expect.hasAssertions();
-    const myArray = getListStudents();
-    expect(myArray[0]).toStrictEqual({ id: 1, firstName: 'Guillaume', location: 'San Francisco' });
-  });
-});
