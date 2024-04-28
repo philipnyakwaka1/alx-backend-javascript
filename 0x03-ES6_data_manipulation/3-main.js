30d8d3970d2420bdd80df500205662ca8d18708a
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-04-28 11:12:07 +0300

    return new set

--- 0x03-ES6_data_manipulation/3-main.js
+++ 0x03-ES6_data_manipulation/3-main.js
@@ -1,7 +1,3 @@
-import getListStudents from "./0-get_list_students.js";
-import getStudentIdsSum from "./3-get_ids_sum.js";
+import setFromArray from "./6-set.js";
 
-const students = getListStudents();
-const value = getStudentIdsSum(students);
-
-console.log(value);
+console.log(setFromArray([12, 32, 15, 78, 98, 15]));
--- 0x03-ES6_data_manipulation/main.js
+++ 0x03-ES6_data_manipulation/main.js
@@ -1,6 +1,3 @@
-import getListStudents from "./0-get_list_students.js";
-import updateStudentGradeByCity from "./4-update_grade_by_city.js";
+import setFromArray from "./6-set.js";
 
-console.log(updateStudentGradeByCity(getListStudents(), "San Francisco", [{ studentId: 5, grade: 97 }, { studentId: 1, grade: 86 }]));
-
-console.log(updateStudentGradeByCity(getListStudents(), "San Francisco", [{ studentId: 5, grade: 97 }]));
+console.log(setFromArray([12, 32, 15, 78, 98, 15]));
