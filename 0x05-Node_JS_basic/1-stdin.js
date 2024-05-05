edb2555cf8c355ef865c077e357587de0e3a0887
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-05-05 10:52:32 +0300

    process.stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,16 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
-  if (process.stdin.isTTY) {
+
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
     process.exit();
-  }
-});
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing.\n');
-});
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
