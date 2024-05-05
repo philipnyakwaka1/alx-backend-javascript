419133c1e291ca6cde97874ad3961d3250b7f2b6
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-05-05 10:47:13 +0300

    process.stdin

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,9 @@
+/**
+ * A function that displays a message to the console.
+ *
+ * @param {string} message - The message to be displayed.
+ * @return {undefined} 
+ */
 function displayMessage(message) {
   console.log(message);
 }
